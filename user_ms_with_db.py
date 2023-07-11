33bf639772a83ebc939af1a40a7ccd50a19b7825
Keshav Upadhyaya <keshavupadhyaya@Keshavs-MacBook-Air.local>
2023-07-11 08:26:24 +0200

    format

--- user_ms_with_db.py
+++ user_ms_with_db.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 import sqlite3
 import hashlib
 import base64
@@ -7,6 +7,7 @@ from flask_cors import CORS
 count_http_request = 0
 
 app = Flask(__name__)
+
 
 def is_sha1(maybe_sha):
     if len(maybe_sha) != 40:
@@ -21,115 +22,129 @@ def is_sha1(maybe_sha):
 def isBase64(sb):
     try:
         if type(sb) == str:
-                sb_bytes = bytes(sb, 'ascii')
+            sb_bytes = bytes(sb, 'ascii')
         elif type(sb) == bytes:
-                sb_bytes = sb
+            sb_bytes = sb
         else:
-                raise ValueError("Argument must be string or bytes")
+            raise ValueError("Argument must be string or bytes")
         return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
     except Exception:
         return False
 
+
 def add_count():
-	global count_http_request
-	count_http_request+=1
+    global count_http_request
+    count_http_request += 1
+
 
 app = Flask(__name__)
 api = CORS(app)
 
+
 @app.errorhandler(405)
 def func(e):
-	add_count()
-	return "",405
-	
-@app.route('/api/v1/_count',methods=['GET'])
-def http_count():
-    if(request.method=='GET'):
-        return jsonify(list([count_http_request])),200
-    else:
-        return jsonify({"message":"Method not allowed"}),405
+    add_count()
+    return "", 405
 
-@app.route('/api/v1/_count',methods=['DELETE'])
+
+@app.route('/api/v1/_count', methods=['GET'])
+def http_count():
+    if (request.method == 'GET'):
+        return jsonify(list([count_http_request])), 200
+    else:
+        return jsonify({"message": "Method not allowed"}), 405
+
+
+@app.route('/api/v1/_count', methods=['DELETE'])
 def http_count_reset():
     global count_http_request
-    if(request.method=='DELETE'):
-        count_http_request=0
-        return jsonify({}),200
+    if (request.method == 'DELETE'):
+        count_http_request = 0
+        return jsonify({}), 200
     else:
-        return jsonify({"message":"Method not allowed"}),405
+        return jsonify({"message": "Method not allowed"}), 405
 
-#1 Add user--------------------------------------------------------------------
-@app.route('/api/v1/users',methods=['POST'])
+# 1 Add user--------------------------------------------------------------------
+
+
+@app.route('/api/v1/users', methods=['POST'])
 def add_user():
     add_count()
-    if(request.method=='POST'):        
+    if (request.method == 'POST'):
         with sqlite3.connect("users.db") as connectionState:
-            cursor=connectionState.cursor()
-            
+            cursor = connectionState.cursor()
+
             if not request.json or not 'username' in request.json or not 'password' in request.json:
-                return jsonify({"message":"username or password missing"}),400
-            
+                return jsonify({"message": "username or password missing"}), 400
+
             username = request.json['username']
             password = request.json['password']
-    
-            users=cursor.execute("select Username from User")
-            users=list(users)
-            users=[users[i][0] for i in range(0,len(users))]
-            	
-            if username in users:
-                return jsonify({"message":"user {} already exists".format(username)}),400
-            
-            if not is_sha1(request.json['password']):
-                return jsonify({"message":"password not in sha1 format. Enter a proper format"}),400
-            
-            cursor.execute("insert into User values (?, ?)",(username,password))
-            return jsonify({"message":"User {} added".format(request.json['username'])}),201
-    else:
-        return jsonify({"message":"Method not allowed"}),405
 
-#2 Remove user-----------------------------------------------------------------
-@app.route('/api/v1/users/<username>',methods=['DELETE'])
+            users = cursor.execute("select Username from User")
+            users = list(users)
+            users = [users[i][0] for i in range(0, len(users))]
+
+            if username in users:
+                return jsonify({"message": "user {} already exists".format(username)}), 400
+
+            if not is_sha1(request.json['password']):
+                return jsonify({"message": "password not in sha1 format. Enter a proper format"}), 400
+
+            cursor.execute("insert into User values (?, ?)",
+                           (username, password))
+            return jsonify({"message": "User {} added".format(request.json['username'])}), 201
+    else:
+        return jsonify({"message": "Method not allowed"}), 405
+
+# 2 Remove user-----------------------------------------------------------------
+
+
+@app.route('/api/v1/users/<username>', methods=['DELETE'])
 def remove_user(username):
     add_count()
-    if(request.method=='DELETE'):
+    if (request.method == 'DELETE'):
         with sqlite3.connect("users.db") as connectionState:
-            cursor=connectionState.cursor()
-            users=cursor.execute("select Username from User")
-            users=list(users)
+            cursor = connectionState.cursor()
+            users = cursor.execute("select Username from User")
+            users = list(users)
             print(username)
             print(users)
-            users=[users[i][0] for i in range(0,len(users))]
+            users = [users[i][0] for i in range(0, len(users))]
             if username in users:
-                cursor.execute("delete from User where Username=(?)",(username,))
-                return jsonify({"message":"user {} removed".format(username)}),200
+                cursor.execute(
+                    "delete from User where Username=(?)", (username,))
+                return jsonify({"message": "user {} removed".format(username)}), 200
             else:
-                return jsonify({"message":"User doesn't exist"}),400       
+                return jsonify({"message": "User doesn't exist"}), 400
     else:
-        return jsonify({"message":"Method not allowed"}),405
+        return jsonify({"message": "Method not allowed"}), 405
 
-#3 List all users--------------------------------------------------------------
-@app.route('/api/v1/users',methods=['GET'])
+# 3 List all users--------------------------------------------------------------
+
+
+@app.route('/api/v1/users', methods=['GET'])
 def list_all_users():
     add_count()
-    if(request.method=='GET'):
+    if (request.method == 'GET'):
         with sqlite3.connect("users.db") as connectionState:
-            cursor=connectionState.cursor()
-            users=cursor.execute("select Username from User")
-            users=list(users)
+            cursor = connectionState.cursor()
+            users = cursor.execute("select Username from User")
+            users = list(users)
             print(users)
-            users=[users[i][0] for i in range(0,len(users))]  
-            if(users==[]):
-                return jsonify({"message":"No users"}),204
+            users = [users[i][0] for i in range(0, len(users))]
+            if (users == []):
+                return jsonify({"message": "No users"}), 204
             else:
-                return jsonify(users),200
+                return jsonify(users), 200
     else:
-        return jsonify({"message":"Method not allowed"}),405
+        return jsonify({"message": "Method not allowed"}), 405
 
 
 @app.route('/')
 def home():
-	return "SelfieLessActs user management microservice is working!\nOwner: CC_078_125_127_165"
+    return "SelfieLessActs user management microservice is working!\nOwner: CloudComp2"
+
 
 if __name__ == '__main__':
-    #app.run(debug=True,port=8000)
-    app.run(host="0.0.0.0",port=80)
+    # app.run(debug=True,port=8000)
+    app.run(host="0.0.0.0", port=80)
