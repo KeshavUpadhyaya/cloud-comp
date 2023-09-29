91f858f0fed6d1acf6a13c469c75e90b8f6c54bb
Viren Gajera <gajeraviren07@gmail.com>
2023-09-29 16:18:12 +0200

    sup

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-
-#Viren test
-#Find a way to create VPC ID and use it in code
-#Figure out how our auto scaling is working
-#Add a new feature
 terraform {
   required_providers {
     aws = {
@@ -13,7 +8,23 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1" 
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "selected_subnet" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b"]
+  }
 }
 
 resource "tls_private_key" "flask_app_keypair" {
@@ -58,12 +69,11 @@ resource "aws_security_group" "flask_app_sg" {
   }
 }
 
+# standalone instance for testing (not part of autoscaling group)
 resource "aws_instance" "flask_app" {
   ami           = "ami-0261755bbcb8c4a84" # Ubuntu 20.04 LTS image in us-east-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.flask_app_keypair.key_name
-
-  vpc_security_group_ids = [aws_security_group.flask_app_sg.id]
 
   tags = {
     Name = "FlaskAppInstance"
@@ -88,30 +98,25 @@ resource "aws_instance" "flask_app" {
 
 # Define an Auto Scaling Group with a default launch configuration.
 resource "aws_autoscaling_group" "flask_app_asg" {
-  name                      = "flask-app-asg"
-  min_size                  = 2
-  max_size                  = 5
-  desired_capacity          = 2
-  availability_zones        = ["us-east-1a", "us-east-1b"]
-  launch_configuration      = aws_launch_configuration.flask_app.name
-  target_group_arns         = [aws_lb_target_group.flask_app.arn]
-  health_check_type         = "ELB"
-
-  lifecycle { 
-    ignore_changes = [desired_capacity, target_group_arns]
-  }
-
+  name                 = "flask-app-asg"
+  min_size             = 2
+  max_size             = 5
+  desired_capacity     = 2
+  availability_zones   = ["us-east-1a", "us-east-1b"]
+  launch_configuration = aws_launch_configuration.flask_app.name
+  target_group_arns    = [aws_lb_target_group.flask_app.arn]
+  health_check_type    = "ELB"
 }
 
 # Define a launch configuration for the Auto Scaling Group.
 resource "aws_launch_configuration" "flask_app" {
-  name_prefix = "flask-app-"
-  image_id    = "ami-0261755bbcb8c4a84"
-  instance_type            = "t2.micro"
-  key_name                 = aws_key_pair.flask_app_keypair.key_name
+  name_prefix                 = "flask-app-"
+  image_id                    = "ami-0261755bbcb8c4a84"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.flask_app_keypair.key_name
   associate_public_ip_address = true
-  security_groups          = [aws_security_group.flask_app_sg.id]
-  user_data = <<-EOF
+  security_groups             = [aws_security_group.flask_app_sg.id]
+  user_data                   = <<-EOF
               #!/bin/bash
               apt update
               apt install -y docker.io
@@ -130,11 +135,12 @@ resource "aws_launch_configuration" "flask_app" {
 
 # Define a load balancer.
 resource "aws_lb" "flask_app_lb" {
-  name               = "flask-app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.flask_app_sg.id]
+  name                       = "flask-app-lb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.flask_app_sg.id]
   enable_deletion_protection = false
+  subnets                    = data.aws_subnets.selected_subnet.ids
 }
 
 # Define a target group for the load balancer.
@@ -143,6 +149,7 @@ resource "aws_lb_target_group" "flask_app" {
   port        = 80
   protocol    = "HTTP"
   target_type = "instance"
+  vpc_id      = data.aws_vpc.default.id
 }
 
 # Create a listener for the load balancer.
@@ -152,7 +159,7 @@ resource "aws_lb_listener" "flask_app" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "fixed-response"
+    type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
       status_code  = "200"
@@ -176,40 +183,33 @@ resource "aws_lb_listener_rule" "flask_app" {
   }
 }
 
+# Setting up autoscaling alaram
+resource "aws_cloudwatch_metric_alarm" "requests_alarm" {
+  alarm_name          = "requests-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization" # Replace with your custom metric name
+  namespace           = "AWS/EC2" # Replace with your custom namespace
+  period              = 60         # 1-minute period
+  statistic           = "Average"
+  threshold           = 0
+  alarm_description   = "Scale up when requests exceed 10 per minute"
+  alarm_actions       = [aws_autoscaling_policy.scale_up_policy.arn]
+  dimensions = {
+    YourDimensionName = "count" # Replace with your dimension name and value
+  }
+}
+
+resource "aws_autoscaling_policy" "scale_up_policy" {
+  name                   = "scale-up-policy"
+  scaling_adjustment     = 1 # Increase desired capacity by 1 instance/container
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300 # Cooldown period in seconds
+  autoscaling_group_name = aws_autoscaling_group.flask_app_asg.name
+}
+
 output "load_balancer_public_ip" {
   value = aws_lb.flask_app_lb.dns_name
-}
-
-
-resource "aws_autoscaling_attachment" "flask_app" {
-  autoscaling_group_name = aws_autoscaling_group.flask_app_asg.id
-  lb_target_group_arn   = aws_lb_target_group.flask_app.id
-}
-
-
-resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "flask_app_scale_up"
-  autoscaling_group_name = aws_autoscaling_group.flask_app_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = 1
-  cooldown               = 120
-}
-
-resource "aws_cloudwatch_metric_alarm" "scale_up" {
-  alarm_description   = "Monitors CPU utilization for Terramino ASG"
-  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
-  alarm_name          = "flask_app_alarm_scale_up"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  namespace           = "AWS/EC2"
-  metric_name         = "CPUUtilization"
-  threshold           = "10"
-  evaluation_periods  = "2"
-  period              = "120"
-  statistic           = "Average"
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.flask_app_asg.name
-  }
 }
 
 
